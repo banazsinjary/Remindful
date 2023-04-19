dff08f1bf7b5be54ae7d1ac30b2c2fcfb7195e54
banaz <banazsinjary@gmail.com>
2023-04-18 20:23:24 -0700

    Set up table view for to do list w/o attributes

--- Remindful/ToDoView.swift
+++ Remindful/ToDoView.swift
@@ -1,5 +1,38 @@
 import UIKit
 
-class ToDoView: UIViewController{
+struct Item {
+    let text: String
+    let isDone: Bool
+}
+
+class ToDoView: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    var items: [Item] = [
+        Item(text: "wake up at 8am", isDone: false),
+        Item(text: "mindfulness", isDone: false)
+    ]
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func newItemButton(_ sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            //code here
+        }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return items.count
+        }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        return cell
+        }
     
 }
