c7eea42db179777ed2fd6579fad3067bdaea50f9
banaz <banazsinjary@gmail.com>
2023-05-11 18:36:26 -0700

    custom habit cell

---
+++ Remindful/HabitCell.swift
@@ -0,0 +1,40 @@
+//
+//  HabitCell.swift
+//  Remindful
+//
+//  Created by Banaz Sinjary on 5/11/23.
+//
+
+import UIKit
+
+class HabitCell: UITableViewCell {
+
+    @IBOutlet weak var habitLabel: UILabel!
+    
+    @IBOutlet var dayButtons: [UIButton]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func dayButtonTapped(_ sender: UIButton) {
+        let tag = sender.tag
+        for button in dayButtons{
+            if button.tag == tag{
+                button.backgroundColor = UIColor.black
+                button.setTitleColor(.black, for: .normal)
+            }
+            else{
+                //stays the same
+            }
+        }
+    }
+    
+}
--- Remindful/HabitView.swift
+++ Remindful/HabitView.swift
@@ -1,9 +1,41 @@
 import UIKit
 
-class HabitView: UIViewController {
+struct Habit{
+    let text: String
+    let days: [Bool]
+}
+
+
+class HabitView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var habits: [Habit]=[
+        Habit(text: "yoga practice", days: [false]),
+        Habit(text: "run 2 miles", days: [false])
+    ]
     
     @IBOutlet weak var habitTable: UITableView!
-    @IBOutlet weak var habitName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        habitTable.dataSource = self
+        habitTable.delegate = self
+        habitTable.register(UINib(nibName: "HabitCell", bundle: nil), forCellReuseIdentifier: "HabitCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return habits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = habitTable.dequeueReusableCell(withIdentifier: "HabitCell", for: indexPath) as! HabitCell
+        let habit = habits[indexPath.row]
+        cell.habitLabel?.text = habit.text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
     
     
 }
